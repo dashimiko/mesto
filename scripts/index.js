9aa0e55116d41614c3ae0ac93b535ff0335d7278
Daria Mikolaichuk <dashamikolaichuk@gmail.com>
2022-01-20 13:38:13 +0300

    последняя версия с попапом

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+//открытие и закрытие попапа
+
 const profileOpenPopupButton = document.querySelector('.info__edit-button');
 const popup = document.querySelector('.popup');
 const popupCloseButton = document.querySelector('.popup__close');
@@ -12,3 +14,28 @@ function closePopup() {
 
 profileOpenPopupButton.addEventListener('click', openPopup);
 popupCloseButton.addEventListener('click', closePopup);
+
+//редактирование формы
+
+// Находим форму в DOM
+let formElement = document.forms.form;
+let nameInput = formElement.name;
+let jobInput = formElement.description;
+let inputName = nameInput.value;
+let inputJob = jobInput.value;
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+
+
+let inputs = document.querySelectorAll('input');
+
+console.log(inputs[0].value);
+console.log(inputs[1].value);
+
+nameInput.textContent = inputName;
+jobInput.textContent = inputJob;
+closePopup();
+}
+
+formElement.addEventListener('submit' , formSubmitHandler);
