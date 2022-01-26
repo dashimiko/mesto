725274f8eb3a25033ebc75b8eaab4bed5911720c
Daria Mikolaichuk <dashamikolaichuk@gmail.com>
2022-01-26 13:18:34 +0300

    корректировки после первого ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,21 @@
-//открытие и закрытие попапа
-
-const profileOpenPopupButton = document.querySelector('.info__edit-button');
+const profileOpenPopupButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const popupCloseButton = document.querySelector('.popup__close');
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.getElementById('name');
+let jobInput = document.getElementById('description');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__description');
 
 function openPopup() {
   popup.classList.add('popup_opened');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
 }
 
 function closePopup() {
   popup.classList.remove('popup_opened');
 }
-
-profileOpenPopupButton.addEventListener('click', openPopup);
-popupCloseButton.addEventListener('click', closePopup);
-
-//редактирование формы
-
-let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__name');
-let jobInput = document.querySelector('.popup__description');
-let profileName = document.querySelector('.info__name');
-let profileJob = document.querySelector('.info__description');
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
@@ -31,3 +25,5 @@ function formSubmitHandler (evt) {
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
+profileOpenPopupButton.addEventListener('click', openPopup);
+popupCloseButton.addEventListener('click', closePopup);
