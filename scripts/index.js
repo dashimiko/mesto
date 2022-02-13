b419239e8ae4673f27b933d5eaedcf7345730af4
Daria Mikolaichuk <dashamikolaichuk@gmail.com>
2022-02-13 19:50:00 +0300

    create:валидация форм

--- scripts/index.js
+++ scripts/index.js
@@ -81,13 +81,13 @@ function openProfilePopup (){
   openPopup(profilePopup);
 };
 
-//функция, определяющая место карточек к доме
+//функция, рендерящая карточки
 function renderCards(card) {
   const cardElement = createCard(card);
   elements.append(cardElement);
 }
 
-//функция, рендерящая карточки
+//функция, перебирающая карточки в массиве
 function render() {
   initialCards.forEach((card) => renderCards(card));
 }
@@ -139,7 +139,6 @@ function addListeners(el) {
 
 //функция увеличения картинки
 function openImage(event) {
-
   fullPictureImage.src = event.target.src
   fullPictureImage.alt = event.target.alt
   fullPictureTitle.textContent = event.target.alt
@@ -172,3 +171,102 @@ document.querySelector('.popup__close_open-picture').addEventListener('click', f
 fullPictureCloseButton.addEventListener('click', function() {
   closePopup(fullPicturePopup);
 });//слушатель закрытия попапа с увеличенной картинкой
+
+
+
+///новый код по тренажеру
+
+// Функция, которая добавляет класс с ошибкой
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add('popup__input_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup__error_visible');
+};
+
+// Функция, которая удаляет класс с ошибкой
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove('popup__input_error');
+  errorElement.classList.remove('popup__error_visible');
+  errorElement.textContent = '';
+};
+
+
+// Функция, которая проверяет валидность поля
+const isValid = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    // Если поле не проходит валидацию, покажем ошибку
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    // Если проходит, скроем
+    hideInputError(formElement, inputElement);
+  }
+};
+
+// Функция принимает массив полей
+
+const hasInvalidInput = (inputList) => {
+  // проходим по этому массиву методом some
+  return inputList.some((inputElement) => {
+    // Если поле не валидно, колбэк вернёт true
+    // Обход массива прекратится и вся фунцкция
+    // hasInvalidInput вернёт true
+
+    return !inputElement.validity.valid;
+  })
+};
+
+const setEventListeners = (formElement) => {
+  // Находим все поля внутри формы,
+  // сделаем из них массив методом Array.from
+  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+  const buttonElement = formElement.querySelector('.popup__submit-button');
+  toggleButtonState(inputList, buttonElement);
+
+  // Обойдём все элементы полученной коллекции
+  inputList.forEach((inputElement) => {
+    // каждому полю добавим обработчик события input
+    inputElement.addEventListener('input', () => {
+      // Внутри колбэка вызовем isValid,
+      // передав ей форму и проверяемый элемент
+      isValid(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+
+const enableValidation = () => {
+  // Найдём все формы с указанным классом в DOM,
+  // сделаем из них массив методом Array.from
+  const formList = Array.from(document.querySelectorAll('.popup__form'));
+
+  // Переберём полученную коллекцию
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', (evt) => {
+      // У каждой формы отменим стандартное поведение
+      evt.preventDefault();
+    });
+
+    // Для каждой формы вызовем функцию setEventListeners,
+    // передав ей элемент формы
+    setEventListeners(formElement);
+  });
+};
+
+// Вызовем функцию
+enableValidation();
+
+// Функция принимает массив полей ввода
+// и элемент кнопки, состояние которой нужно менять
+
+function toggleButtonState (inputList,buttonElement) {
+  if(hasInvalidInput(inputList)) {
+    buttonElement.classList.add('popup__submit-button_inactive')
+  } else {
+    buttonElement.classList.remove('popup__submit-button_inactive')
+  }
+
+}
+
